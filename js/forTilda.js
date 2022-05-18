bcf797462c6b29068945c23695a0f193adab6c8a
kenshi244 <92234802+kenshi244@users.noreply.github.com>
2022-05-18 22:55:46 +0300

    Add files via upload

---
+++ js/forTilda.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+            alert("Script started working");
+
+            function findMin() {
+                minInput = document.querySelector('.js-store-filter-pricemin')
+            }
+
+            function delay(ms) {
+                return new Promise((resolve, reject) => {
+                    setTimeout(resolve, ms);
+                });
+            }
+            let catalog, minInput;
+            catalog = document.querySelector('#rec446610068');
+            while (!catalog) {
+                setTimeout(minInput = document.querySelector('#rec446610068'), 1000);
+            }
+            if (catalog != null) alert("Catalog");
+
+            delay(5000).then(findMin());
+            while (minInput == null) {
+                delay(2000).then(findMin());
+            }
+
+            if (minInput != null) alert(`Min input: ${minInput}`)
+
+            minInput.addEventListener("change", function () {
+                alert(`MIN INPUT VALUE: ${minInput.value}`);
+            })
+        });
